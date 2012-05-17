581300de151c711aa1ab628811471a9b3845a43b
Bryan Marty <bxm156@case.edu>
2012-05-17 05:06:22 +0000

    disable debugging on production

--- GreenButtonLauncher/settings.py
+++ GreenButtonLauncher/settings.py
@@ -1,8 +1,8 @@
 # Django settings for GreenButtonLauncher project.
 import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+DEBUG = False
+TEMPLATE_DEBUG = False
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
