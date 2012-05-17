a4ff119218ff6963c80515f2a90c748e063b79a3
Bryan Marty <bxm156@case.edu>
2012-05-17 00:37:12 -0400

    Added disclaimer… FAQ on challenge site says its needed >.<

--- GreenButtonLauncher/apps/gbcontent/urls.py
+++ GreenButtonLauncher/apps/gbcontent/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('GreenButtonLauncher.apps.gbcontent.views',
     url(r'^$', 'start', name="start"),
+    url(r'^disclaimer/$', 'disclaimer'),
     url(r'ajax-upload$', 'import_uploader', name="my_ajax_upload"),
 )
